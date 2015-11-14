ef8c6b96c5062fd1512f72eb3ed24e9840c2d159
watch3602004 <watch3602004@yahoo.co.jp>
2015-11-14 18:33:57 +0900

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,16 @@
+package sph9_iga;
+
+public class Room {
+	//constants
+	
+	//fields
+	protected int[] member;
+	protected int num;
+	
+	//constructor
+	Room(int pop){
+		member=new int[pop];
+	}
+	
+	//methods	
+}
